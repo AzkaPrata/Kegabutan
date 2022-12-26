451827dd9658978c82bfc030fc23e7f0e023f17a
AzkaPrata <96645707+AzkaPrata@users.noreply.github.com>
2022-12-26 11:40:14 +0700

    Hehe, Akhirnya Bisa Bikin PJ lagi

---
+++ README.md
@@ -0,0 +1,10 @@
+This Is **KEGABUTAN** azka
+
+**Created With** 
+
+![jQuery](https://img.shields.io/badge/jquery-0769AD.svg?style=for-the-badge&logo=jquery&logoColor=white)
+
+![Bootstrap](https://img.shields.io/badge/bootstrap-7952B3.svg?style=for-the-badge&logo=bootstrap&logoColor=white)
+
+
+> @AzkaPrata Cimahi 2022
---
+++ indes.js
@@ -0,0 +1,75 @@
+
+
+let angka = 0
+const kata=[
+    "Itung Lagi", "Cape Dah", "Cape Yah...", 
+    "Udahan Jig", "Jangan Diterusin", 
+    "Mending Itung Dosa", "Istigfar",
+    "Mending Dzikir Dah", "Dah Sore Dzikir Sore",
+    "Baca Qur'an Gih", "Udahan", "Error", "Error",
+    "Error Error"
+]
+
+$(".clicker").click(function click(){
+    angka++
+    console.log(angka)
+    $(".angka").text(angka)
+    if(angka == 10){
+        $(".title").text(kata[Math.floor(Math.random() * 15)])
+    }else if(angka == 20){
+        $(".title").text(kata[Math.floor(Math.random() * 15)])
+    }else if(angka == 30){
+        $(".title").text(kata[Math.floor(Math.random() * 15)])
+    }else if(angka == 40){
+        $(".title").text(kata[Math.floor(Math.random() * 15)])
+    }else if(angka == 50){
+        $(".title").text(kata[Math.floor(Math.random() * 15)])
+    }
+    else if(angka == 60){
+        $(".title").text(kata[Math.floor(Math.random() * 15)])
+    }
+    else if(angka == 70){
+        $(".title").text(kata[Math.floor(Math.random() * 15)])
+    }
+    else if(angka == 80){
+        $(".title").text(kata[Math.floor(Math.random() * 15)])
+    }
+    else if(angka == 90){
+        $(".title").text(kata[Math.floor(Math.random() * 15)])
+    }
+    else if(angka == 100){
+        $(".title").text(kata[Math.floor(Math.random() * 15)])
+    }else if(angka > 100){
+        Swal.fire({
+            title: 'Error',
+            text: "Yah Kebanyakan, HAPUS!!!",
+            icon: 'error',
+            showCancelButton: true,
+            confirmButtonText: 'Yes',
+            cancelButtonText: 'No',
+            reverseButtons: true
+          }).then((result) => {
+            if (result.isConfirmed) {
+              Swal.fire(
+                'Kehapus!!!',
+                'Skormu Telah Terhapus',
+                'success'
+              )
+              angka = 0
+              $(".angka").text(angka)
+            } else if (
+              /* Read more about handling dismissals below */
+              result.dismiss === Swal.DismissReason.cancel
+            ) {
+              Swal.fire(
+                'Gak Jadi Deh',
+                ['____ ____',
+                 '==== ====',
+                
+                '==========='],
+                'error'
+              )
+            }
+          })
+    }})
+
